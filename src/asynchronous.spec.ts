fe0e8942e6e2a7478d4cca9d333fd174e950a335
Mari Vanzuit <marileize@gmail.com>
2024-02-14 18:48:16 +1100

    Fix all code style offenses

--- src/asynchronous.spec.ts
+++ src/asynchronous.spec.ts
@@ -1,11 +1,13 @@
-import { asyncFunc } from "./asynchronous"
+import { asyncFunc } from "./asynchronous";
 
-test('Testing async function', async () => {
-    expect(await asyncFunc("John")).toBe("Hello John")
-})
+test("Testing async function", async () => {
+  expect(await asyncFunc("John")).toBe("Hello John");
+});
 
-test('Using promises', () => {
-    asyncFunc("Mary")
-        .then((result) => expect(result).toBe("Hello Mary"))
-        .catch((err) => { throw err })
-})
+test("Using promises", () => {
+  asyncFunc("Mary")
+    .then((result) => expect(result).toBe("Hello Mary"))
+    .catch((err) => {
+      throw err;
+    });
+});
--- src/asynchronous.ts
+++ src/asynchronous.ts
@@ -1,3 +1,3 @@
-export async function asyncFunc(name:string) {
-   return "Hello " + name
+export async function asyncFunc(name: string) {
+  return "Hello " + name;
 }
