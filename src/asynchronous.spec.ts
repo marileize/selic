b78e24d13098c2606328b0ae0d7ed4396ed86b81
Mari Vanzuit <marileize@gmail.com>
2024-02-14 17:59:30 +1100

    Add asynchronous example

---
+++ src/asynchronous.spec.ts
@@ -0,0 +1,11 @@
+import { asyncFunc } from "./asynchronous"
+
+test('Testing async function', async () => {
+    expect(await asyncFunc("John")).toBe("Hello John")
+})
+
+test('Using promises', () => {
+    asyncFunc("Mary")
+        .then((result) => expect(result).toBe("Hello Mary"))
+        .catch((err) => { throw err })
+})
