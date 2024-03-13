ace3c23bb3589d095e9b97a0ccfd47f32bfaba10
Mari Vanzuit <marileize@gmail.com>
2024-03-13 17:00:40 +1100

    Pass the API params in a record

--- src/selic-entries.ts
+++ src/selic-entries.ts
@@ -1,10 +1,11 @@
 export async function selicEntries(initialDate: string, finalDate: string) {
-    const url = new URL('https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json');
-    const params = new URLSearchParams([
-        ['dataInicial', initialDate],
-        ['dataFinal', finalDate],
-    ]);
-    url.search = params.toString();
+    const url = new URL(
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json",
+    );
+    url.search = new URLSearchParams({
+        dataInicial: initialDate,
+        dataFinal: finalDate,
+    }).toString();
     
     const response = await fetch(url.toString());
     const selic_entries = await response.json();
