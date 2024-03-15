5f8e81bfbc51bbd5084ee615e6587f07b69639ce
Mari Vanzuit <marileize@gmail.com>
2024-03-15 19:19:55 +1100

    Convert selicEntries parameters to date objects

--- src/selic-entries.spec.ts
+++ src/selic-entries.spec.ts
@@ -2,10 +2,16 @@ import { selicEntries } from "./selic-entries";
 
 describe("selicEntries", () => {
     test("Successful request for a given period", async () => {
-        expect(await selicEntries("08/03/2024","08/03/2024")).toEqual([
+        expect(
+            await selicEntries(new Date("2024-03-08"), new Date("2024-03-11")),
+        ).toEqual([
             {
-                "data": '08/03/2024',
-                "valor": '0.041957',
+                "data":"08/03/2024",
+                "valor":"0.041957",
+            },
+            {
+                "data":"11/03/2024",
+                "valor":"0.041957",
             },
         ]);
     });
--- src/selic-entries.ts
+++ src/selic-entries.ts
@@ -1,10 +1,13 @@
-export async function selicEntries(initialDate: string, finalDate: string) {
+import { format, compareAsc } from "date-fns"
+
+export async function selicEntries(initialDate: Date, finalDate: Date) {
     const url = new URL(
         "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json",
     );
+
     url.search = new URLSearchParams({
-        dataInicial: initialDate,
-        dataFinal: finalDate,
+        dataInicial: format(initialDate, "dd/MM/yyyy"),
+        dataFinal: format(finalDate, "dd/MM/yyyy"),
     }).toString();
     
     const response = await fetch(url.toString());
