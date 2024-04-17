5a087b31d0e8a2dff44207a721f3d009aa91be37
Mari Vanzuit <marileize@gmail.com>
2024-04-17 15:11:35 +1000

    Add beforeEach and afterEach blocks to setup mocks

--- src/selic-entries.validations.spec.ts
+++ src/selic-entries.validations.spec.ts
@@ -2,31 +2,34 @@ import { selicEntries } from "./selic-entries";
 import fetchMock from "jest-fetch-mock";
 
 describe("selicEntries", () => {
-  test("Throws error for an initial date before Selic start date", async () => {
+  beforeEach(() => {
     fetchMock.enableMocks();
+  });
+
+  afterEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  test("Throws error for an initial date before Selic start date", async () => {
     await expect(() =>
       selicEntries(new Date("1900-06-05"), new Date("2024-03-11")),
     ).rejects.toThrow("Initial date cannot be before 4/06/1986");
   });
 
   test("Throws error for an initial date before Selic start date", async () => {
-    fetchMock.enableMocks();
     await expect(() =>
       selicEntries(new Date("1900-06-05"), new Date("2024-03-11")),
     ).rejects.toThrow("Initial date cannot be before 4/06/1986");
   });
 
   test("Initial date cannot be after final date", async () => {
-    fetchMock.enableMocks();
     await expect(() =>
       selicEntries(new Date("2024-03-12"), new Date("2024-03-11")),
     ).rejects.toThrow("Initial date cannot be after final date");
   });
 
   test("Empty dates", async () => {
-    fetchMock.mockResponse(() =>
-      selicEntries().then((res) => "ok"),
-    );
+    fetchMock.mockResponse(() => selicEntries().then((res) => "ok"));
   });
 
   test("Initial date empty", async () => {
